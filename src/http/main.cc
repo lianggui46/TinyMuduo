2d7158b7f69b026ffe20abb8e9a02e4ab2b544be
lianggui <1921433598@qq.com>
2023-06-26 13:42:40 +0800

    fix image bug

--- src/http/main.cc
+++ src/http/main.cc
@@ -13,6 +13,9 @@
 #include <sys/uio.h>
 #include <fcntl.h>
 
+#define likely(x) __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(!!(x), 0)
+
 bool benchmark = true;
 
 void cb(const HttpRequest& req, HttpResponse* resp)
@@ -55,7 +58,7 @@ void cb(const HttpRequest& req, HttpResponse* resp)
         struct stat m_file_stat;
 
         // 获取文件的相关的状态信息，-1失败，0成功
-        if ( stat(filename.c_str(), &m_file_stat ) < 0 ) {
+        if (unlikely( ::stat(filename.c_str(), &m_file_stat ) < 0 )) {
             resp->setStatusCode(HttpResponse::k404NotFound);
             resp->setStatusMessage("Not Found");
             resp->setCloseConnection(true);
@@ -68,35 +71,63 @@ void cb(const HttpRequest& req, HttpResponse* resp)
             resp->setCloseConnection(true);
             return;
         }
-        //查询文件是否为文件夹
-        if ( S_ISDIR( m_file_stat.st_mode ) ){
+
+        // 以只读方式打开文件
+        int fd = open(filename.c_str(), O_RDONLY );
+        if(unlikely(fd < 0)){
             resp->setStatusCode(HttpResponse::k400BadRequest);
             resp->setStatusMessage("Bad Request");
             resp->setCloseConnection(true);
             return;
         }
 
-        // 以只读方式打开文件
-        int fd = open(filename.c_str(), O_RDONLY );
         // 创建内存映射
-        char* m_file_address = ( char* )mmap(0, m_file_stat.st_size, PROT_READ, MAP_PRIVATE, fd, 0 );
-        close(fd);
-        if(m_file_address == MAP_FAILED){
+        void* m_file_address = ::mmap(0, m_file_stat.st_size, PROT_READ, MAP_PRIVATE, fd, 0 );
+        
+        if(filename.c_str() == "/home/lianggui/tinymuduo/src/http/resources/images/image1.jpg"){
+            std::cout << m_file_address << std::endl;
+            std::cout << m_file_stat.st_size << std::endl;
+        }
+
+        if(unlikely(m_file_address == MAP_FAILED)){
             resp->setStatusCode(HttpResponse::k400BadRequest);
             resp->setStatusMessage("Bad Request");
             resp->setCloseConnection(true);
             LOG_ERROR << "MAP_FAILED";
+            return;
         }
         resp->setStatusCode(HttpResponse::k200Ok);
         resp->setStatusMessage("OK");
-        resp->setContentType("text/html");
-        resp->addHeader("Server", "Muduo");
-        resp->setBody(std::string(m_file_address, m_file_address + m_file_stat.st_size));
 
-        if(m_file_address){
-            munmap( m_file_address, m_file_stat.st_size );
-            m_file_address = NULL;
+        const std::string & rp = req.path();
+        int idx = rp.find_last_of('.');
+
+        if(idx == std::string::npos){
+            resp->setContentType("text/plain"); 
         }
+
+        else{
+            std::string fmt = rp.substr(idx);
+            if(fmt == ".html"){
+                resp->setContentType("text/html"); 
+            }
+            else if(fmt == ".png"){
+                resp->setContentType("image/png");
+            }
+            else if(fmt == ".jpg"){
+                resp->setContentType("image/jpeg");
+            }
+            else{
+                resp->setContentType("text/html");
+            }
+        }
+        
+        resp->addHeader("Server", "Muduo");
+        resp->setBody(std::string(static_cast<char *>(m_file_address), m_file_stat.st_size));
+
+        ::close(fd);
+        munmap( m_file_address, m_file_stat.st_size );
+        m_file_address = NULL;
     }
 }
 
